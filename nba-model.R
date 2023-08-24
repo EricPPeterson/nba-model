b4431dea07197897bdf7cf27abb2efd831a0d537
Eric Peterson <EricPPeterson@users.noreply.github.com>
2023-08-24 21:31:43 +1000

    Update nba-model.R

--- nba-model.R
+++ nba-model.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(tidybayes)
 library(lubridate)
 
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
 game_data <- game_logs(seasons = c(2020:2022), season_types = 'Regular Season', result_types = c('team', 'player'))
 teams_data <- teams_tables(teams = c('Boston Celtics'), seasons = 2022, tables = c('splits', 'shooting'),
-                           measures = c('Advanced'), modes = c('Per48'))
+                           measures = c('Base'), modes = c('Per48'))
