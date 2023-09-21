9f7c76587b74127d516f6e521a16cab7ffdd00d8
Eric Peterson <EricPPeterson@users.noreply.github.com>
2023-09-21 14:57:32 +1000

    Update nba-model.R

--- nba-model.R
+++ nba-model.R
@@ -1,10 +1,35 @@
-library(nbastatR)
+####################################################################################################################
+#NBA 2023
+####################################################################################################################
+library(hoopR)
 library(tidyr)
 library(dplyr)
 library(tidybayes)
 library(lubridate)
 
-Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
-game_data <- game_logs(seasons = c(2020:2022), season_types = 'Regular Season', result_types = c('team', 'player'))
-teams_data <- teams_tables(teams = c('Boston Celtics'), seasons = 2022, tables = c('splits', 'shooting'),
-                           measures = c('Base'), modes = c('Per48'))
+nba_pbp <- load_nba_pbp(2017:2021) %>%
+  progressr::with_progress()
+schedule <- load_nba_schedule(2017:2021)
+
+#count possessions changes
+nba_pbp$team_id <- as.numeric(nba_pbp$team_id)
+
+nba_pbp <- nba_pbp[complete.cases(nba_pbp$team_id),]
+
+nba_pbp <- nba_pbp %>%
+  group_by(game_id) %>%
+  mutate(change = cumsum(c(0,as.numeric(diff(team_id))!=0))) %>%
+  ungroup()
+
+poss_check <- nba_pbp %>%
+  dplyr::select(game_id, change, team_id, type_id, type_text)
+
+game_1 <- poss_check %>%
+  dplyr::filter(game_id == 400954514)
+###################################################################################################################
+#max possessions by team / season
+###################################################################################################################
+poss_count_game <- nba_pbp %>%
+  group_by(game_id) %>%
+  summarize(sum_poss = max(change)) %>%
+  mutate(team_poss = sum_poss / 2)
